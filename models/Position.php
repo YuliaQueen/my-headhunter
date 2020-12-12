1b5ab46f4def8071834c55bf8e474def6060d1b1
Yulia <youlka25@yandex.ru>
2020-12-12 18:50:26 +0300

    новый дамп базы

---
+++ models/Position.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "position".
+ *
+ * @property int $id
+ * @property string $position_title
+ *
+ * @property Resume[] $resumes
+ */
+class Position extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'position';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['position_title'], 'required'],
+            [['position_title'], 'string', 'max' => 250],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'position_title' => 'Position Title',
+        ];
+    }
+
+    /**
+     * Gets query for [[Resumes]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getResumes()
+    {
+        return $this->hasMany(Resume::className(), ['position' => 'id']);
+    }
+}
